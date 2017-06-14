f61721a7b6bb98c9530fbc9359bfa2206236fbd4
awalqui <walqang@gmail.com>
2017-06-14 14:25:08 -0400

    Toggle Comment on Btn Click

--- src/ArticleLinks.js
+++ src/ArticleLinks.js
@@ -6,21 +6,30 @@ class ArticleLinks extends Component {
     super()
 
     this.state = {
-      showComments: true
+      showComments: false
     }
+
+    this.toggleComments = this.toggleComments.bind(this)
+  }
+
+  toggleComments(ev) {
+    ev.preventDefault()
+    this.setState({showComments: !this.state.showComments})
   }
 
   render() {
     return (
-      <div className="article-links">
-        <a className="article-link" href="#">
-          <i className="fa fa-comments-o"></i>
-          <span className="article-link-text">Comments</span>
-        </a>
-        <a className="article-link" href="#">
-          <i className="fa fa-share"></i>
-          <span className="article-link-text">Share Post</span>
-        </a>
+      <div>
+        <div className="article-links">
+          <a className="article-link" href="#" onClick={this.toggleComments}>
+            <i className="fa fa-comments-o"></i>
+            <span className="article-link-text">Comments</span>
+          </a>
+          <a className="article-link" href="#">
+            <i className="fa fa-share"></i>
+            <span className="article-link-text">Share Post</span>
+          </a>
+        </div>
         {this.state.showComments ? <p>Comments!</p> : null}
       </div>
     )
