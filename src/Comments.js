c2553aeae2e227440c6ddc1cf93c16f5d843f9bb
awalqui <walqang@gmail.com>
2017-06-14 14:45:32 -0400

    update and add commment to state

--- src/Comments.js
+++ src/Comments.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react'
 
 class Comments extends Component {
-    constructor() {
-        super()
+  constructor() {
+    super()
 
-        this.state = {
-            comment: ''
-        }
-
-        this.updateComment = this.updateComment.bind(this)
+    this.state = {
+      comment: '',
+      comments: []
     }
 
-    updateComment(ev) {
-        this.setState({
-            commend: ev.target.value
-        })
-    }
+    this.updateComment = this.updateComment.bind(this)
+    this.addComment = this.addComment.bind(this)
+  }
 
-    render() {
-        return (
-            <div className="comments">
-                <textarea
-                    value={this.state.comment}
-                    onChange={this.updateComment}
-                    placeholder="Enter Comment">
-                </textarea>
-                <button className="button">Submit comment</button>
-            </div>
-        )
+  updateComment(ev) {
+    this.setState({
+      comment: ev.target.value
+    })
+  }
+
+  addComment(ev) {
+    const comment = {
+      timestamp: new Date(),
+      text: this.state.comment
     }
+    const state = {...this.state}
+    state.comments.push(comment)
+    state.comment = ''
+    this.setState(state, () => console.log(this.state))
+  }
+
+  render() {
+    return (
+      <div className="comments">
+        <textarea
+          value={this.state.comment}
+          onChange={this.updateComment}
+          placeholder="Enter comment here">
+        </textarea>
+        <button onClick={this.addComment} className="button">Submit comment</button>
+      </div>
+    )
+  }
 }
 
 export default Comments
