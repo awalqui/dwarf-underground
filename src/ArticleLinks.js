cdde6810efdd941b4e08af6782d442ed69ce6024
awalqui <walqang@gmail.com>
2017-06-14 14:12:26 -0400

    Conditionally show comments

--- src/ArticleLinks.js
+++ src/ArticleLinks.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react'
 import './ArticleLinks.css'
 
 class ArticleLinks extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      showComments: true
+    }
+  }
+
   render() {
     return (
-            <div className="article-links">
-              <a className="article-link" href="#">
-                <i className="fa fa-comments-o"></i>
-                <span className="article-link-text">Comments</span>
-              </a>
-              <a className="article-link" href="#">
-                <i className="fa fa-share"></i>
-                <span className="article-link-text">Share Post</span>
-              </a>
-            </div>
+      <div className="article-links">
+        <a className="article-link" href="#">
+          <i className="fa fa-comments-o"></i>
+          <span className="article-link-text">Comments</span>
+        </a>
+        <a className="article-link" href="#">
+          <i className="fa fa-share"></i>
+          <span className="article-link-text">Share Post</span>
+        </a>
+        {this.state.showComments ? <p>Comments!</p> : null}
+      </div>
     )
   }
 }
