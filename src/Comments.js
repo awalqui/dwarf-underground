7f07bfa197657ab7f4ae97bc80f593335275173a
awalqui <walqang@gmail.com>
2017-06-14 15:28:31 -0400

    color swap on comment button

--- src/Comments.js
+++ src/Comments.js
@@ -1,47 +1,58 @@
 import React, { Component } from 'react'
+import "./Comments.css"
 
 class Comments extends Component {
-  constructor() {
-    super()
+    constructor() {
+        super()
 
-    this.state = {
-      comment: '',
-      comments: []
+        this.state = {
+            comment: '',
+            comments: []
+        }
+
+        this.updateComment = this.updateComment.bind(this)
+        this.addComment = this.addComment.bind(this)
     }
 
-    this.updateComment = this.updateComment.bind(this)
-    this.addComment = this.addComment.bind(this)
-  }
-
-  updateComment(ev) {
-    this.setState({
-      comment: ev.target.value
-    })
-  }
-
-  addComment(ev) {
-    const comment = {
-      timestamp: new Date(),
-      text: this.state.comment
+    updateComment(ev) {
+        this.setState({
+            comment: ev.target.value
+        })
     }
-    const state = {...this.state}
-    state.comments.push(comment)
-    state.comment = ''
-    this.setState(state, () => console.log(this.state))
-  }
 
-  render() {
-    return (
-      <div className="comments">
-        <textarea
-          value={this.state.comment}
-          onChange={this.updateComment}
-          placeholder="Enter comment here">
-        </textarea>
-        <button onClick={this.addComment} className="button">Submit comment</button>
-      </div>
-    )
-  }
+    addComment(ev) {
+        const comment = {
+            timestamp: new Date(),
+            text: this.state.comment
+        }
+        const state = { ...this.state }
+        state.comments.push(comment)
+        state.comment = ''
+        this.setState(state)
+    }
+
+    render() {
+        return (
+            <div className="comments">
+                <textarea
+                    value={this.state.comment}
+                    onChange={this.updateComment}
+                    placeholder="Enter comment here">
+                </textarea>
+                <button onClick={this.addComment} className="button">Submit comment</button>
+                {this.state.comments.map((comment, i) => <Comment comment={comment} key={i} />)}
+            </div>
+        )
+    }
 }
+
+function Comment(props) {
+    return (
+        <div className="comment">
+            <div>{props.comment.text}</div>
+        </div>
+    )
+}
+
 
 export default Comments
