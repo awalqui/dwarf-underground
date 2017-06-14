adf8f4cdaeaf4ed97fb4dfffddd81db558ee67e6
awalqui <walqang@gmail.com>
2017-06-13 20:32:57 -0400

    Something went horribly wrong so I backtracked

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import Header from './Header'
-import ArticleBody from './ArticleBody'
-import Footer from './Footer'
 
 class App extends Component {
   render() {
@@ -20,7 +19,21 @@ class App extends Component {
                 <p className="date">on 28 April, 3018 of the Third Age</p>
               </div>
             </div>
-            <ArticleBody />
+            <section className="article-body">
+              <p><span className="article-start gold">E</span>psum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
+
+              <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
+
+              <p>Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Gratuitous octopus niacin, sodium glutimate.</p>
+
+              <p className="interject">Avast! A stray goblin!</p>
+
+              <p>Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito.</p>
+
+              <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
+
+              <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
+            </section>
             <div className="article-links">
               <a className="article-link" href="#">
                 <i className="fa fa-comments-o"></i>
@@ -41,11 +54,9 @@ class App extends Component {
               </div>  
               <h4>Mining diamonds or crushing the skulls of your enemies, the SmashMaster 4000 does it all</h4>
               <p className="ad-text">Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
-
               <p className="ad-text">Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
             </div>
           </aside>
-
           <div className="small-12 columns other-articles">
             <h2>From around the Realm</h2>
             <div className="small-6 medium-3 columns other-article">
@@ -74,11 +85,12 @@ class App extends Component {
             </div>
           </div>
         </main>
-
-        <Footer />
+        <footer className="expanded row">
+          <h6>&copy; 2017 Erebor Industries</h6>
+          <h6 className="float-right">All rights reserved</h6>
+        </footer>
       </div>
     );
   }
 }
-
 export default App;
