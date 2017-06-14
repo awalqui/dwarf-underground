c50d84caa3e2b643b56d1cc93ce52c9c85cdf869
awalqui <walqang@gmail.com>
2017-06-14 14:36:16 -0400

    hande comment updates

--- src/Comments.js
+++ src/Comments.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react'
 
 class Comments extends Component {
-  render() {
-    return (
-      <div className="comments">
-        <textarea></textarea>
-        <button className="button">Submit comment</button>
-      </div>
-    )
-  }
+    constructor() {
+        super()
+
+        this.state = {
+            comment: ''
+        }
+
+        this.updateComment = this.updateComment.bind(this)
+    }
+
+    updateComment(ev) {
+        this.setState({
+            commend: ev.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="comments">
+                <textarea
+                    value={this.state.comment}
+                    onChange={this.updateComment}
+                    placeholder="Enter Comment">
+                </textarea>
+                <button className="button">Submit comment</button>
+            </div>
+        )
+    }
 }
 
 export default Comments
